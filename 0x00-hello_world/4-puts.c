9d1a0ec1aa0e0befd6795b577dd1d5a9babc020d
obikiel <ezekielobiomachi1@gmail.com>
2021-06-20 15:38:55 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+/**
+*main - print the string in the put function
+*
+*Description: using the main function
+*this program prints "Programming is like building a multilingualpuzzle"
+* Return: 0
+*/
 int main(void)
- {
-    puts("\"Programming is like building a multilingual puzzle ");
-    return (0);
- }
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
