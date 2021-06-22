7dd6592a8a66bf059f79dbb2bdb7a781a4682c19
obikiel <ezekielobiomachi1@gmail.com>
2021-06-22 12:08:45 -0700

    commit

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+*main - function to print
+*
+*Return: 0
+*/
+int main(void)
+{
+char holberton[9] = {'H', 'o', 'l', 'e', 'r', 't', 'o', 'n'};
+unsigned int c;
+for (c = 0; c < sizeof(holberton); c++)
+{
+_putchar(holberton[c]);
+}
+_putchar('\n');
+return (0);
+}
